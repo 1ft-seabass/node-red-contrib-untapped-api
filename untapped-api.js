55e415914ebe5d095075445ea19c1e5ffc4e7c71
Tanaka Seigo <seigo@1ft-seabass.jp>
2021-06-04 10:39:04 +0900

    update

---
+++ untapped-api.js
@@ -0,0 +1,38 @@
+var request = require('request');
+
+module.exports = function (RED) {
+
+  function CredAddSettingConfig(config) {
+    RED.nodes.createNode(this, config);
+    var node = this;
+    this.value1 = config.value1;
+  }
+  RED.nodes.registerType("untapped-api-config", CredAddSettingConfig, {
+    credentials: {
+      apitoken: {type:"text"},
+      password: {type:"password"}
+    }
+  });
+
+  function CredAddSetting(config) {
+    RED.nodes.createNode(this, config);
+
+    this.current_setting = RED.nodes.getNode(config.setting);
+    this.value1 = this.current_setting.value1;
+    this.apitoken = this.current_setting.credentials.apitoken;
+    this.password = this.current_setting.credentials.password;
+
+    console.log("this.current_setting", this.current_setting);
+
+    console.log("config.setting",config.setting);
+    console.log("this.apitoken",this.apitoken);
+    console.log("this.password",this.password);
+    console.log("this.value1",this.value1);
+
+    var node = this;
+    node.on('input', function (msg) {
+      node.send(msg);
+    });
+  }
+  RED.nodes.registerType("untapped-api", CredAddSetting, {});
+}
